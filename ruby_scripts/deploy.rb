2505bf023c03e5bc94cc5ab72c75b1ba722d97ad
AnaTofuZ <anatofuz@gmail.com>
2019-09-11 14:22:30 +0900

    環境によってinitializeする変数を設定

--- ruby_scripts/deploy.rb
+++ ruby_scripts/deploy.rb
@@ -24,8 +24,33 @@ class CoderDojoSakuraCLI
   end
 
   def run()
-    pp @isSandbox
+    request_params = perform_init_params()
+    p request_params
   end
+
+  def perform_init_params
+    if @isSandbox
+      init_production_params()
+    else
+      init_sandbox_params()
+    end
+  end
+
+  def init_production_params
+    {
+     zone: "31002", # 石狩第二
+     api: "https://secure.sakura.ad.jp/cloud/zone/is1b/api/cloud/1.1/", # 石狩第二
+     packetfilterid: '112900922505', # See https://secure.sakura.ad.jp/cloud/iaas/#!/network/packetfilter/.
+    }
+  end
+  def init_sandbox_params
+    {
+     zone: "29001", # サンドボックス
+     api: "https://secure.sakura.ad.jp/cloud/zone/tk1v/api/cloud/1.1/",
+     packetfilterid: '112900927419', # See https://secure.sakura.ad.jp/cloud/iaas/#!/network/packetfilter/.
+    }
+  end
+ 
 end
 
 CoderDojoSakuraCLI.new(ARGV).run()
