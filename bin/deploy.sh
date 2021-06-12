7cad896ec6fa266658738c76ff6a18ab157fc926
Shigenobu Nishikawa <shishi@srevo.net>
2021-06-12 16:07:17 +0900

    use add -force in deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -21,8 +21,8 @@ bundle exec ruby ruby_scripts/deploy.rb
 git config user.name "Github Actions"
 git config user.email "noreply@yasslab.jp"
 
-git add README.md
-git add instances.csv
+git add -f README.md
+git add -f instances.csv
 
 git commit --quiet -m "Deploy from actions"
 git push --force --quiet "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}" master:gh-pages
