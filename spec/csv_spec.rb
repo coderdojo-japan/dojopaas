b6236eb98feb5b1ac3cbf9e0825b6a07e757db28
Yohei Yasukawa <yohei@yasslab.jp>
2024-02-15 11:41:56 +0900

    Cosmetic change on spec/csv_spec.rb

--- spec/csv_spec.rb
+++ spec/csv_spec.rb
@@ -3,12 +3,12 @@ require 'csv'
 INSTANCE_CSV = "servers.csv".freeze
 
 describe 'valid CSV' do
-    CSV.read(INSTANCE_CSV,headers: true).each do |line|
-      it {
-        expect(line['name']).to_not be nil
-        expect(line['description']).to_not be nil
-        expect(line['pubkey']).to_not be nil
-        expect(line['branch']).to_not be nil
-      }
+  CSV.read(INSTANCE_CSV,headers: true).each do |line|
+    it {
+      expect(line['name']       ).to_not be nil
+      expect(line['description']).to_not be nil
+      expect(line['pubkey']     ).to_not be nil
+      expect(line['branch']     ).to_not be nil
+    }
   end
 end
