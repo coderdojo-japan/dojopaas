d4f953c5e89143c1316daa5f0c4bd704c3417afc
Yohei Yasukawa <yohei@yasslab.jp>
2024-02-15 11:53:12 +0900

    Change user.name and user.email to existing user

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,11 +4,11 @@ set -e
 
 bundle exec ruby scripts/deploy.rb 
 
-git config user.name "Github Actions"
-git config user.email "noreply@yasslab.jp"
+git config user.name  "Yohei Yasukawa"
+git config user.email "yohei@yasslab.jp"
 
 git add -f README.md
 git add -f instances.csv
 
-git commit --quiet -m "Deploy from actions"
+git commit --quiet -m    "Deploy from actions"
 git push --force --quiet "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}" master:gh-pages
